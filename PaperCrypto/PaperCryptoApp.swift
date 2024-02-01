15ba969e909c5816b576b574a7bef103bcbca01f
Tushar Patil <SwiftyTushar@gmail.com>
2024-02-01 08:41:25 +0530

    Initial Commit

---
+++ PaperCrypto/PaperCryptoApp.swift
@@ -0,0 +1,20 @@
+//
+//  PaperCryptoApp.swift
+//  PaperCrypto
+//
+//  Created by Tushar Patil on 01/02/24.
+//
+
+import SwiftUI
+
+@main
+struct PaperCryptoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
